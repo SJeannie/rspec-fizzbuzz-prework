5ba7256dbde75b59ef113e4e9af160ceb445cbf8
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-13 16:11:35 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(num)
+
+  output = nil  
+  
+  if num % 3 == 0 
+    output = "Fizz"   
+  
+  if (output == "Fizz" && num % 5 == 0) 
+    output = "FizzBuzz"
+  
+  elsif (num % 5 == 0) 
+    output = "Buzz"
+  
+  return output
+  
+end
